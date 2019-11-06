ab9b24d23d6a223b6347c821747bc8971a4d121e
KennHuang <kennhung@users.noreply.github.com>
2019-11-06 15:31:07 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,27 +6,23 @@ git clone https://github.com/kennhung/rc-files.git
 mkdir rc_old
 
 read -r -p "Install .vimrc? [y/N] " response
-case $response in
-    [yY][eE][sS]|[yY])
-		# install vimrc
-		cd rc-files
-		find ../.vimrc -exec mv ../{} ../rc_old/ \;
-		cd ..
-		ln -s rc-files/.vimrc .
-        ;;
-    *)
-esac
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	# install vimrc
+	cd rc-files
+	find ../.vimrc -exec mv ../{} ../rc_old/ \;
+	cd ..
+	ln -s rc-files/.vimrc .
+fi
 
 read -r -p "Install .tmux.conf? [y/N] " response
-case $response in
-    [yY][eE][sS]|[yY])
-                # install vimrc
-                cd rc-files
-                find ../.tmux.conf -exec mv ../{} ../rc_old/ \;
-                cd ..
-                ln -s rc-files/.tmux.conf .
-        ;;
-    *)
-esac
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	# install vimrc
+        cd rc-files
+        find ../.tmux.conf -exec mv ../{} ../rc_old/ \;
+        cd ..
+        ln -s rc-files/.tmux.conf .
+fi
 
 
