765f5c0a241619b8a7d400b243445e88b51cd086
KennHuang <kennhung@users.noreply.github.com>
2019-11-06 15:37:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,24 +5,10 @@ git clone https://github.com/kennhung/rc-files.git
 
 mkdir rc_old
 
-read -r -p "Install .vimrc? [y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-	# install vimrc
-	cd rc-files
-	find ../.vimrc -exec mv ../{} ../rc_old/ \;
-	cd ..
-	ln -s rc-files/.vimrc .
-fi
+cd rc-files
+find ../.vimrc -exec mv ../{} ../rc_old/ \;
+find ../.tmux.conf -exec mv ../{} ../rc_old/ \;
 
-read -r -p "Install .tmux.conf? [y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-	# install vimrc
-        cd rc-files
-        find ../.tmux.conf -exec mv ../{} ../rc_old/ \;
-        cd ..
-        ln -s rc-files/.tmux.conf .
-fi
-
-
+cd ..
+ln -s rc-files/.vimrc .
+ln -s rc-files/.tmux.conf .
