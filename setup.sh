baf596146000292ff2908dc069b66d33e7c6c8b7
KennHuang <kennhung@users.noreply.github.com>
2020-02-15 10:55:07 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ mkdir rc_old
 cd rc-files
 find ../.vimrc -exec mv ../{} ../rc_old/ \;
 find ../.tmux.conf -exec mv ../{} ../rc_old/ \;
+find ../.zshrc -exec mv ../{} ../rc_old/ \;
 
 cd ..
 ln -s rc-files/.vimrc .
 ln -s rc-files/.tmux.conf .
+ln -s rc-files/.zshrc .
